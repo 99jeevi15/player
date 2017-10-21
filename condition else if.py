3a83b3157f3a457f0851c5b9b0d9320c1454b635
99jeevi15 <32951415+99jeevi15@users.noreply.github.com>
2017-10-21 13:54:57 +0530

    Add files via upload

---
+++ condition else if.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse013
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse013 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=int(input("enter the age"))
+if a<20:
+  print("not eligible for a")
+else:
+  print("eligible for a")

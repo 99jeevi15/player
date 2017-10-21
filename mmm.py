398d1e2a7df9b5ca0a2e50440fb43d79f13af8a2
99jeevi15 <32951415+99jeevi15@users.noreply.github.com>
2017-10-21 12:16:44 +0530

    Add files via upload

---
+++ mmm.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse013
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse013 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+stringvar="programmer"
+v=stringvar.count('r')
+print(v)
+v=stringvar.find('g')
+print(v)
+v=stringvar.replace('m','y')
+print(v)
